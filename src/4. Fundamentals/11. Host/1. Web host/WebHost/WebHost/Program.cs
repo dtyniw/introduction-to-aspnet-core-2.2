1d01420e5648f5ef2852e39e93ad46adb80c2e8e
Dominik <copyhere@wp.pl>
2019-02-20 21:52:18 +0100

    Fundamentals | Host | Web host

---
+++ src/4. Fundamentals/11. Host/1. Web host/WebHost/WebHost/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebHostSample
+{
+    /// <summary>
+    /// Override Configuration
+    /// To specify the host run on a particular URL, the desired value can be passed in from a command prompt when executing dotnet run. The command-line argument overrides the urls value from the hostsettings.json file, and the server listens on port 8080:
+    /// <code>dotnet run --urls "http://*:8080"</code>
+    /// </summary>
+    /// <see cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/web-host?view=aspnetcore-2.2"/>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hostsettings.json", optional: true)
+                .AddCommandLine(args)
+                .Build();
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5000")
+                .UseConfiguration(config)
+                .Configure(app =>
+                {
+                    app.Run(context =>
+                        context.Response.WriteAsync("Hello, World!"));
+                });
+        }
+    }
+}

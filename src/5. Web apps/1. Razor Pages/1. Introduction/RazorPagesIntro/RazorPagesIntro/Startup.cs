71cdbb05364a8a0a51b6f49e27bdadbf604b2aaf
Dominik <copyhere@wp.pl>
2019-02-21 14:19:09 +0100

    Web apps | Razor Pages | Introduction
    RazorPagesIntro C# project

---
+++ src/5. Web apps/1. Razor Pages/1. Introduction/RazorPagesIntro/RazorPagesIntro/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RazorPagesIntro
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Includes support for Razor Pages and controllers.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}

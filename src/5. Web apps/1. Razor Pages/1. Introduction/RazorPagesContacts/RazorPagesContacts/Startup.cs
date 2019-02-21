f501fbcbaf6c45b171bb7a13cd13abe103380b0e
Dominik <copyhere@wp.pl>
2019-02-21 21:56:06 +0100

    Web apps | Razor Pages | Introduction
    RazorPagesContacts C# project

---
+++ src/5. Web apps/1. Razor Pages/1. Introduction/RazorPagesContacts/RazorPagesContacts/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesContacts.Data;
+
+namespace RazorPagesContacts
+{
+    public class Startup
+    {
+        public IHostingEnvironment HostingEnvironment { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                              options.UseInMemoryDatabase("name"));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}

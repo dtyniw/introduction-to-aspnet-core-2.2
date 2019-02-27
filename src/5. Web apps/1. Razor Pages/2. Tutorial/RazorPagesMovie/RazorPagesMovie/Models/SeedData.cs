54ac38ff8c3c42b1baef1821451050586ec4d336
Dominik <copyhere@wp.pl>
2019-02-27 21:14:50 +0100

    Web apps | Razor Pages | Tutorial | Add a new field

--- src/5. Web apps/1. Razor Pages/2. Tutorial/RazorPagesMovie/RazorPagesMovie/Models/SeedData.cs
+++ src/5. Web apps/1. Razor Pages/2. Tutorial/RazorPagesMovie/RazorPagesMovie/Models/SeedData.cs
@@ -10,8 +10,7 @@ namespace RazorPagesMovie.Models
         public static void Initialize(IServiceProvider serviceProvider)
         {
             using (var context = new RazorPagesMovieContext(
-                serviceProvider.GetRequiredService<
-                    DbContextOptions<RazorPagesMovieContext>>()))
+                serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>()))
             {
                 // Look for any movies.
                 if (context.Movie.Any())
@@ -19,21 +18,25 @@ namespace RazorPagesMovie.Models
                     return;   // DB has been seeded
                 }
 
+                #region snippet1
                 context.Movie.AddRange(
                     new Movie
                     {
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
-                        Price = 7.99M
+                        Price = 7.99M,
+                        Rating = "R"
                     },
+                #endregion
 
                     new Movie
                     {
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
-                        Price = 8.99M
+                        Price = 8.99M,
+                        Rating = "G"
                     },
 
                     new Movie
@@ -41,7 +44,8 @@ namespace RazorPagesMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
-                        Price = 9.99M
+                        Price = 9.99M,
+                        Rating = "G"
                     },
 
                     new Movie
@@ -49,7 +53,8 @@ namespace RazorPagesMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
-                        Price = 3.99M
+                        Price = 3.99M,
+                        Rating = "NA"
                     }
                 );
                 context.SaveChanges();

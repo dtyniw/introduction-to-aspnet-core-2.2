9947157586f0280da717cde8b15f3e2c2ea0c854
Dominik <copyhere@wp.pl>
2019-02-27 19:35:54 +0100

    Web apps | Razor Pages | Tutorial | Update the pages

--- src/5. Web apps/1. Razor Pages/2. Tutorial/RazorPagesMovie/RazorPagesMovie/Pages/Movies/Delete.cshtml.cs
+++ src/5. Web apps/1. Razor Pages/2. Tutorial/RazorPagesMovie/RazorPagesMovie/Pages/Movies/Delete.cshtml.cs
@@ -25,10 +25,12 @@ namespace RazorPagesMovie.Pages.Movies
         {
             if (id == null)
             {
-                return NotFound();
+                Movie = await _context.Movie.FirstOrDefaultAsync();
             }
-
-            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+            else
+            {
+                Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+            }
 
             if (Movie == null)
             {
